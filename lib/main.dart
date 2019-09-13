9906ec3940355d5426d369047b5c1e2f9f73c5b2
Marcelo Torrez <elmac395@gmail.com>
2019-09-12 20:54:22 -0400

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
+//En Flutter el primer paso siempre es importar las bibliotecas que se usarán
+//En este caso llamamos a la biblioteca de material.dart la cual no permitirá
+//utilizar los widgets de Material Design
 import 'package:flutter/material.dart';
 
+//El método 'main' en la clase main.dart no permite inicializar una aplicación
 void main() => runApp(MyApp());
 
+//Creamos la clase MyApp la cual hereda de StatelessWidget, Esta clase nos
+//permitirá instanciar el Scafoold (Linea )
 class MyApp extends StatelessWidget {
 
+  //Este widget nos permite reutilizar el estilo de un texto para no tener que
+  //escribirlo una y otra vez, así mismo recibe un parametro
   Widget textoPrincipal(String text){
+    //Se retorna un Widget de tipo texto
     return Text(
+      //Se define caul será el contenido de este widget Text
       text,
+      //Se define el estilo que tendrá nuestro widget Text
       style: TextStyle(
           color: Colors.white,
           fontSize: 20.0,
@@ -15,11 +26,18 @@ class MyApp extends StatelessWidget {
     );
   }
 
+  //De igual manera este widget nos permitirá reutilizar código para no tenerlo
+  //que escribir una y otra vez
   Widget componenteUno(String name){
+    //En este caso el widget retorna una columna
     return Column(
+      //Acá se instancian los widgets hijos que contiene esta columna
       children: <Widget>[
+        //Este es un widget de tipo Icon el cual no dibuja un icono en la pantalla
         Icon(
+          //Seleccionamos el icono que querramos mostrar
           Icons.audiotrack,
+          //Definimos el color del icono
           color: Color(0xFF388E3C),
         ),
         Text(
@@ -48,38 +66,65 @@ class MyApp extends StatelessWidget {
       ],
     );
   }
-  // This widget is the root of your application.
+  // Este widget es la raiz de la aplicación.
   @override
+  //El metodo build es el encargado de construir el widget en este caso
+  // retornará una MaterialApp
   Widget build(BuildContext context) {
     return MaterialApp(
+      //Este es el título que la aplicación presenta al verse en segundo plano
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+      //Este es el tema de la aplicación, Tando colores como el contraste que
+      //que tendran los textos en widgets como el appBar, ect.
+        theme: ThemeData(
+        //Se define un atributo del tema, en este caso el color primario de la aplicación
         primarySwatch: Colors.green,
       ),
+      //El atributo 'home' es el encargado de generar la estructura base de la
+      //aplicación ya que contiene el widget principal de la aplicación el 'Scaffold'
+
+        /*El Scaffold es el widget que contendra todos los componentes(widgets) de la aplicación
+        * entre ellos los principales son el AppBar y el Body*/
       home: Scaffold(
+        /*El appBar sera nuestra barra de herramientas donde podremos poner un menu
+        * el título y algunos items que se puedan necesitar*/
         appBar: AppBar(
+          /*El title es el título que tendra el appBar y recibe un widget de texto*/
           title: Text(
             "Mi primera app",
           ),
         ),
+        /*El body será el contenedor de tolo el contenido que querramos mostrar
+        * en nuestra aplicación*/
+
+        /*El ListView es un widget que nos permite poner varios widgets uno detras del
+        otro como si se tratará de un Column pero tiene  la caracteristica
+        de generar un SCROLL en la pantalla el cual evitara que el contenido
+        de la pantalla sea limitado*/
         body: ListView(
           children: <Widget>[
+            /*Column es un widget que nos permite agrupar varios otros widgets
+            * en forma vertical (En coluna)*/
             Column(
+              /*El atributo children nos permite instanciar la cantidad de widgets
+              * 'hijos' que querramos estructurar en nuestra columna*/
               children: <Widget>[
+                /*El Container es un widget que nos permitira darle una dimención
+                * especifica a cualquier widget que tenga como hijo en este caso
+                * una fila, de la misma forma podemos personalizar el contenedor como
+                * mejor nos parezca Linea: */
                 Container(
                   height: 40.0,
                   color: Color(0xFF388E3C),
+                  /*El widget Center nos permite posicionar un widget hijo al medio del
+                  * widget padre*/
                   child: Center(
+                    /*El widget Row al igual que el Column nos permite agrupar
+                    * varios widgets hijos pero en este caso en forma de fila*/
                       child: Row(
+                        /*El atributo mainAxisAligment nos permite cambiar la posicion
+                        * entre los widgets hijos del row, asi como poner un espacio
+                        * entre estos*/
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: <Widget>[
                           textoPrincipal("uno"),
